d4067e57d75114d09b886110d9f8cbb40c4f9b78
Maximilian Falkenstein <mfalkenstein@sos.ethz.ch>
2018-05-01 23:22:56 +0200

    Add debian packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 
-
 from distutils.command.build import build
 from setuptools import setup
 import os
 
+
 class CustomBuildCommand(build):
     def run(self):
-        os.system("mibdump.py --mib-source=file://$(pwd) --mib-source http://mibs.snmplabs.com/asn1/@mib@ --destination-directory=check_power e3meter-ipm")
-        build.run(self)
+        # At this point, dependencies should already be installed, so import the compiler only now
+        if os.path.isfile('check_power/NETTRACK-E3METER-SNMP-MIB.py'):
+            print ("File already compiled, skipping...")
+        else:
+            from pysmi.reader import getReadersFromUrls
+            from pysmi.parser import SmiV1CompatParser
+            from pysmi.compiler import MibCompiler
+            from pysmi.searcher.stub import StubSearcher
+            from pysmi.writer.pyfile import PyFileWriter
+            from pysmi.codegen.pysnmp import PySnmpCodeGen, baseMibs
+            mibSources = [os.path.abspath(os.path.dirname('./e3meter-ipm.mib')), 'http://mibs.snmplabs.com/asn1/@mib@']
+            mibCompiler = MibCompiler(SmiV1CompatParser(), PySnmpCodeGen(),
+                                      PyFileWriter(os.path.join(os.path.abspath(os.path.dirname('./e3meter-ipm.mib')), "check_power")))
+
+            mibCompiler.addSources(*getReadersFromUrls(*mibSources))
+            mibCompiler.addSearchers(StubSearcher(*baseMibs))
+
+            results = mibCompiler.compile(os.path.abspath('./e3meter-ipm.mib'))
+            print(results)
+            build.run(self)
+
 
 setup(name='check_power',
       version='0.1',
@@ -23,4 +42,4 @@ setup(name='check_power',
           'build': CustomBuildCommand
       },
       zip_safe=False
-     )
+)
